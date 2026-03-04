813339c41a32682889af6191df007db13652720b
aurthur001-oss <aurthur001-oss@users.noreply.github.com>
2026-03-05 01:18:08 +0530

    Final compliance fix: Corrected imports and updated migrations.

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from django.views.generic import CreateView, View, DetailView, UpdateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.core.mail import send_mail
